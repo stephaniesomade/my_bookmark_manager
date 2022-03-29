060d446f34cc1c95c1cc031a93a5d03f727f61f2
stephaniesomade <stephaniesomadee@gmail.com>
2022-03-29 16:47:07 +0100

    wrapping database data in objects

--- app.rb
+++ app.rb
@@ -1,18 +1,29 @@
 require 'sinatra/base'
 require 'sinatra/reloader'
+require './lib/bookmarks'
 
 class BookmarkManager < Sinatra::Base
   configure :development do
     register Sinatra::Reloader
   end 
+
   get '/' do
-    'Bookmark Manager'
+    "Bookmark Manager"
   end
 
   get '/bookmarks'do
     @bookmarks = Bookmark.all
     erb :'bookmarks/index'
   end
+
+  get '/bookmarks/new' do
+    erb :'bookmarks/new'
+  end 
+
+  post '/bookmarks' do
+    Bookmark.create(url: params[:url], title: params[:title])
+    redirect '/bookmarks'
+  end 
 
   run! if app_file == $0
 end
